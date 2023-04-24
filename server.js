46351c3047550e6c76fbaf6bc09fd207c2e3fd5e
tonialtendorf <tvohnoutka05@gmail.com>
2023-04-23 19:26:06 -0500

    updated port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const routes = require('./routes');
 const db = require('./config/connection')
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(express.json());
